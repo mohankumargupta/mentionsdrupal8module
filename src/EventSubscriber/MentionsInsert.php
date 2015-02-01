b123e22daa93a2d01dfd5afc7d5ada165d3cee6c
Mohan Gupta <mohangupta@live.com>
2015-02-01 16:02:31 +1100

    event subscriber working and can execute actions

--- src/EventSubscriber/MentionsInsert.php
+++ src/EventSubscriber/MentionsInsert.php
@@ -14,6 +14,10 @@ return $events;
 public function onMentionsInsert($event) {
 
 // do stuff
+
+$actionManager = \Drupal::service('plugin.manager.action');
+$action = $actionManager->createInstance('user_unblock_user_action');
+$action->execute(false);
 }
 
 }
