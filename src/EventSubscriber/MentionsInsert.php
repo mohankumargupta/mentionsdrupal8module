8f6b43c880802a3408ee09fd706b5893c47b90a4
Mohan Gupta <mohangupta@live.com>
2015-02-02 15:35:01 +1100

    can't get hook_entity_update to fire

--- src/EventSubscriber/MentionsInsert.php
+++ src/EventSubscriber/MentionsInsert.php
@@ -15,9 +15,9 @@ public function onMentionsInsert($event) {
 
 // do stuff
 
-$actionManager = \Drupal::service('plugin.manager.action');
-$action = $actionManager->createInstance('user_unblock_user_action');
-$action->execute(false);
+//$actionManager = \Drupal::service('plugin.manager.action');
+//$action = $actionManager->createInstance('user_unblock_user_action');
+//$action->execute(false);
 }
 
 }
