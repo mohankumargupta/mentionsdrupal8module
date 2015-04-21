4a883c303618d68dc711157cca1f75566e5caf0d
mohankumargupta <mohangupta@live.com>
2015-04-21 10:16:18 +1000

    fixing coding standards for FilterMentions.php

--- src/Plugin/Filter/FilterMentions.php
+++ src/Plugin/Filter/FilterMentions.php
@@ -30,7 +30,7 @@ class FilterMentions extends FilterBase implements ContainerFactoryPluginInterfa
   protected $entityManager;
   protected $renderer;
   protected $config;
-		
+
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, RendererInterface $render, ConfigFactory $config) {
     $this->entityManager = $entity_manager;
     $this->renderer = $render;
@@ -55,7 +55,7 @@ class FilterMentions extends FilterBase implements ContainerFactoryPluginInterfa
   public function setEntityManager($entity_manager) {
     $this->entityManager = $entity_manager;
   }
-  
+
   public function setRenderer($renderer) {
     $this->renderer = $renderer;
   }
@@ -68,7 +68,7 @@ class FilterMentions extends FilterBase implements ContainerFactoryPluginInterfa
     return new FilterProcessResult($this->_filter_mentions($text, $this));
   }
 
-	public function _filter_mentions($text, $filter) {
+  public function _filter_mentions($text, $filter) {
     foreach ($this->mentions_get_mentions($text) as $match) {
       $mentions = array('#theme' => 'mentions', '#user' => $match['user']);
       $mentions2 = $this->renderer->render($mentions);
@@ -77,7 +77,7 @@ class FilterMentions extends FilterBase implements ContainerFactoryPluginInterfa
 
     return $text;
 
-  }
+ }
 
   public function mentions_get_mentions($text) {
     $settings = $this->config->get('mentions.mentions');
@@ -104,34 +104,35 @@ class FilterMentions extends FilterBase implements ContainerFactoryPluginInterfa
     $userStorage = $this->entityManager->getStorage('user');
 
     // Find all matching strings.
-        if (preg_match_all($pattern, $text, $matches, PREG_SET_ORDER)) {
-            foreach ($matches as $match) {
-                if (Unicode::substr($match[2], 0, 1) == '#') {
-                    //$user = user_load(drupal_substr($match[2], 1));
-                    //$user = \Drupal::entityManager()->getStorage('user')->load(Unicode::substr($match[2], 1));
-                    $user = $userStorage->loadByProperties(array('uid'=>Unicode::substr($match[2], 1)));
-                    $user = reset($user);
-                }
-                elseif ($match[1] == '#') {
-                    //$user = user_load($match[2]);
-                    $user = $userStorage->loadByProperties(array('uid'=>$match[2]));
-                    $user = reset($user);
-                }
-                else {
-                    //$user = user_load_by_name($match[2]);
-                    $user = $userStorage->loadByProperties(array('name'=>$match[2]));
-                    $user = reset($user);
-                }
-
-                if (!empty($user)) {
-                    $users[] = array(
-                        'text' => $match[0],
-                        'user' => $user,
-                    );
-                }
-            }
+    if (preg_match_all($pattern, $text, $matches, PREG_SET_ORDER)) {
+      foreach ($matches as $match) {
+        if (Unicode::substr($match[2], 0, 1) == '#') {
+          // $user = user_load(drupal_substr($match[2], 1));
+          // $user = \Drupal::entityManager()->getStorage('user')->load(Unicode::substr($match[2], 1));
+          $user = $userStorage->loadByProperties(array('uid' => Unicode::substr($match[2], 1)));
+          $user = reset($user);
         }
-       
-        return $users;
+        elseif ($match[1] == '#') {
+          // $user = user_load($match[2]);
+          $user = $userStorage->loadByProperties(array('uid' => $match[2]));
+          $user = reset($user);
+        }
+        else {
+          // $user = user_load_by_name($match[2]);
+          $user = $userStorage->loadByProperties(array('name' => $match[2]));
+          $user = reset($user);
+        }
+
+        if (!empty($user)) {
+          $users[] = array(
+            'text' => $match[0],
+            'user' => $user,
+          );
+        }
+      }
     }
+
+    return $users;
+  }
+
 }
