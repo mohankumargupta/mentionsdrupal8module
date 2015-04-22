2cb0dfafda93448f6f003c9bcfb736047d19eaed
mohankumargupta <mohangupta@live.com>
2015-04-22 12:02:22 +1000

    context menu of ckeditor now showing available users when you press @

--- js/plugins/mentionsautocomplete/plugin.js
+++ js/plugins/mentionsautocomplete/plugin.js
@@ -6,9 +6,17 @@
 (function ($, Drupal, drupalSettings, CKEDITOR) {
 
   "use strict";
-  
   CKEDITOR.plugins.add('mentionsautocomplete', {
       init: function (editor) {
+          editor.mentions = [];
+          editor.contextMenu.addListener(function(startElement, selection, path){
+              var editor = selection.root.editor;
+              return editor.mentions.reduce(function(prev,curr,index,array) {
+                  var newValue = prev;
+                  newValue[curr] = CKEDITOR.TRISTATE_OFF;
+                  return newValue;
+              }, {});
+          });
           editor.addCommand('mentionsautocomplete', {
                 modes: {wysiwyg: 1},
                 exec: function(editor) {
@@ -19,12 +27,12 @@
                             var users = JSON.parse(data);
                             var username, userid;
                             
-                            var editorid = editor.name;
-                            var range = editor.getSelection().getRanges()[ 0 ],
-                                rangeRoot = range.root,
-                            startNode = range.startContainer;
-                            var selection = editor.getSelection();
-                            var bookmarks = selection.createBookmarks(true);
+                            //var editorid = editor.name;
+                            //var range = editor.getSelection().getRanges()[ 0 ],
+                            //rangeRoot = range.root,
+                            //startNode = range.startContainer;
+                            //var selection = editor.getSelection();
+                            //var bookmarks = selection.createBookmarks(true);
                          
                             if (editor.contextMenu) {
                                 editor.addMenuGroup('Mentions');
@@ -35,13 +43,24 @@
                                 userid = user.uid;
                                 
                                 if (editor.contextMenu) {
+                                    editor.removeMenuItem('paste');
                                     editor.addMenuItem(username, {
+                                        id: 'mentions_' + userid,
                                         label: username,
                                         group: 'Mentions',
                                         command: 'mentionsautocomplete'
                                     });
                                     
-                                    editor.contextMenu.show(startNode.getParent(),null, 1,30);
+                                    editor.mentions.push(username);
+                                    
+                                    var menuitem = editor.getMenuItem(username);
+                                    
+                                    var range = editor.getSelection().getRanges()[ 0 ];
+                                    var offset = range.endOffset;
+                                    //range.moveToPosition( range.root, CKEDITOR.POSITION_BEFORE_END );
+                                    var startNode = range.endContainer;
+                                    //editor.getSelection().selectRanges( [ range ] );
+                                    editor.contextMenu.show(startNode.getParent(), null,offset*7);
                                 }
                                 
                                 
