2ff2b2932e6cd82907de2e27c62501ce9dfcee10
mohankumargupta <mohangupta@live.com>
2015-05-12 21:00:19 +1000

    added  behat tests for views integration

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,19 +7,20 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Definition\Call;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 
 /**
- * .
+ * 
+.
  *
  * Define application features from the specific context.
  */
 class FeatureContext extends RawDrupalContext implements Context, SnippetAcceptingContext {
   /**
-   * .
+   * 
+.
    *
    * Initializes context.
    * Every scenario gets its own context object.
@@ -42,7 +43,8 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   //  }
   //
   /**
-   * .
+   * 
+.
    *
    * @When I wait :waittime seconds
    */
@@ -51,12 +53,43 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   }
 
   /**
-   * .
    *
    * @When I custom fill content :text into the body field
    */
   public function fillBodyFieldWithText($text) {
     $this->getSession()->evaluateScript("document.querySelector('iframe').contentWindow.document.querySelector('body').innerHTML = '" . $text . "';");
   }
-
+	
+	/**
+	 * @When I enable mentions filter
+	 */
+	public function enableMentionsFilter() {
+		return array(
+			new Call\When('I visit "admin/config/content/formats/manage/basic_html"'),
+			new Call\When('I check "Mentions Filter"'),
+			new Call\When('I press the "Save configuration" button')
+		);
+	}
+	
+	/**
+	 * @When I add page node with title :title with mention :mentiontext 
+	 */
+	public function addNodeWithMention($title,$mentiontext) {
+		
+		return array(
+			new Call\When('I visit "node/add/page"'),
+			new Call\When('I fill in "Title" with "'.$title.'"'),
+			new Call\When('I custom fill content "'.$mentiontext.'" into the body field'),
+			new Call\When('I press the "Save and publish" button')
+		);
+		/*
+		$steps[] = new Call\When('I visit "node/add/page"');
+		$steps[] = new Call\When('I fill in "Title" with "'.$title.'"');
+		$steps[] = new Call\When('I custom fill content "'.$mentiontext.'" into the body field');
+		$steps[] = new Call\When('I press the "Save and publish" button');
+	*/
+		  }
+		 
+	
+	
 }
