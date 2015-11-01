56de12103a1e70b5ba6f32c8cd3aa4d225657ffa
mohankumargupta <mohangupta@live.com>
2015-11-01 22:34:26 +1100

    added filter code

---
+++ src/Plugin/Filter/FilterMentions.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\mentions\Plugin\Filter\FilterMentions.
+ */
+
+namespace Drupal\mentions\Plugin\Filter;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Config\ConfigFactory;
+/**
+ * Class FilterMentions.
+ *
+ * @package Drupal\mentions\Plugin\Filter
+ *
+ * @Filter(
+ * id = "filter_mentions",
+ * title = @Translation("Mentions Filter"),
+ * type = Drupal\filter\Plugin\FilterInterface::TYPE_HTML_RESTRICTOR,
+ * weight = -10
+ * )
+ */
+class FilterMentions extends FilterBase implements ContainerFactoryPluginInterface {
+  protected $entityManager;
+  protected $renderer;
+  protected $config;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, RendererInterface $render, ConfigFactory $config) {
+    $this->entityManager = $entity_manager;
+    $this->renderer = $render;
+    $this->config = $config;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $entity_manager = $container->get('entity.manager');
+    $renderer = $container->get('renderer');
+    $config = $container->get('config.factory');
+
+    return new static($configuration,
+      $plugin_id,
+      $plugin_definition,
+      $entity_manager,
+      $renderer,
+      $config
+    );
+  }
+
+  public function setEntityManager($entity_manager) {
+    $this->entityManager = $entity_manager;
+  }
+
+  public function setRenderer($renderer) {
+    $this->renderer = $renderer;
+  }
+
+  public function setConfig($config) {
+    $this->config = $config;
+  }
+
+  public function process($text, $langcode) {
+    return new FilterProcessResult($this->_filter_mentions($text, $this));
+  }
+
+  public function _filter_mentions($text, $filter) {
+    foreach ($this->mentions_get_mentions($text) as $match) {
+      $mentions = array('#theme' => 'mentions', '#user' => $match['user']);
+      $mentions2 = $this->renderer->render($mentions);
+      $text = str_replace($match['text'], $mentions2, $text);
+    }
+
+    return $text;
+
+  }
+
+  public function mentions_get_mentions($text) {
+    $settings = $this->config->get('mentions.mentions');
+    $users = array();
+
+    // Build regular expression pattern.
+    $pattern = '/(\b|\#)(\w*)/';
+    $input_settings = $settings->get('input');
+
+    switch (TRUE) {
+      case !empty($input_settings['prefix']) && !empty($input_settings['suffix']):
+        $pattern = '/\B(' . preg_quote($input_settings['prefix']) . '|' . preg_quote($this->t($input_settings['prefix'])) . ')(\#?.*?)(' . preg_quote($input_settings['suffix']) . '|' . preg_quote($this->t($input_settings['suffix'])) . ')/';
+        break;
+
+      case !empty($$input_settings['prefix']) && empty($$input_settings['suffix']):
+        $pattern = '/\B(' . preg_quote($$input_settings['prefix']) . '|' . preg_quote($this->t($$input_settings['prefix'])) . ')(\#?\w*)/';
+        break;
+
+      case empty($$input_settings['prefix']) && !empty($$input_settings['suffix']):
+        $pattern = '/(\b|\#)(\w*)(' . preg_quote($$input_settings['suffix']) . '|' . preg_quote($this->t($$input_settings['suffix'])) . ')/';
+        break;
+    }
+
+    $userStorage = $this->entityManager->getStorage('user');
+
+    // Find all matching strings.
+    if (preg_match_all($pattern, $text, $matches, PREG_SET_ORDER)) {
+      foreach ($matches as $match) {
+        if (Unicode::substr($match[2], 0, 1) == '#') {
+          // $user = user_load(drupal_substr($match[2], 1));
+          // $user = \Drupal::entityManager()->getStorage('user')->load(Unicode::substr($match[2], 1));
+          $user = $userStorage->loadByProperties(array('uid' => Unicode::substr($match[2], 1)));
+          $user = reset($user);
+        }
+        elseif ($match[1] == '#') {
+          // $user = user_load($match[2]);
+          $user = $userStorage->loadByProperties(array('uid' => $match[2]));
+          $user = reset($user);
+        }
+        else {
+          // $user = user_load_by_name($match[2]);
+          $user = $userStorage->loadByProperties(array('name' => $match[2]));
+          $user = reset($user);
+        }
+
+        if (!empty($user)) {
+          $users[] = array(
+            'text' => $match[0],
+            'user' => $user,
+          );
+        }
+      }
+    }
+
+    return $users;
+  }
+
+}
+
---
+++ tests/src/Unit/MentionsFilterTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\mentions\Tests\MentionsFilterTest.
+ */
+
+namespace Drupal\mentions\Tests;
+
+use Drupal\mentions\Plugin\Filter\FilterMentions;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coverDefaultClass \Drupal\mentions\Plugin\Filter\FilterMentions
+ * @group Mentions
+ */
+class MentionsFilterTest extends UnitTestCase {
+  /*
+  public static $modules = array('system', 'filter', 'user', 'views', 'views_ui', 'mentions');
+  protected $filters;
+   */
+  protected $entityManager;
+  protected $renderer;
+  protected $userStorage;
+  protected $configFactory;
+  protected $config;
+
+
+  protected function setUp() {
+    parent::setUp();
+
+    $this->userStorage = $this->getMockBuilder('Drupal\Core\Entity\EntityStorageInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $entity_manager = $this->getMock('Drupal\Core\Entity\EntityManagerInterface');
+    $this->entityManager = $entity_manager;
+
+    $renderer = $this->getMock('Drupal\Core\Render\RendererInterface');
+    $this->renderer = $renderer;
+
+    $config = $this->getMock('Drupal\Core\Config\ConfigFactoryInterface');
+    $this->config = $config;
+
+    $configfactory = $this->getMock('Drupal\Core\Config\ConfigFactoryInterface');
+    $this->configFactory = $configfactory;
+  }
+
+  public function testFilterMentionByUsername() {
+    $input = '[@admin]';
+    $username = 'admin';
+    $user = $this->getMockBuilder('Drupal\user\Entity\User')
+                ->disableOriginalConstructor()
+                ->getMock();
+    $expected = array(
+      array(
+        'text' => $input,
+        'user' => $user,
+      ),
+    );
+
+    $inputconfig = array(
+      'prefix' => '[@',
+      'suffix' => ']',
+    );
+
+    $this->userStorage->expects($this->once())
+      ->method('loadByProperties')
+      ->with(array('name' => $username))
+      ->will($this->returnValue(array($user)));
+
+    $this->entityManager->expects($this->once())
+      ->method('getStorage')
+      ->with('user')
+      ->will($this->returnValue($this->userStorage));
+
+    $mentions_filter = $this->getMockBuilder('Drupal\mentions\Plugin\Filter\FilterMentions')
+      ->disableOriginalConstructor()
+      ->setMethods(NULL)
+      ->getMock();
+
+    $mentions_filter->setEntityManager($this->entityManager);
+
+    /*
+    $this->renderer->expects($this->once())
+    ->method('render')
+    ->with($input)
+    ->will($this->returnValue($expected));
+    $mentions_filter->setRenderer($this->renderer);
+     */
+
+    $this->config->expects($this->once())
+      ->method('get')
+      ->with('input')
+      ->will($this->returnValue($inputconfig));
+
+    $this->configFactory->expects($this->once())
+      ->method('get')
+      ->with('mentions.mentions')
+      ->will($this->returnValue($this->config));
+
+    $mentions_filter->setConfig($this->configFactory);
+    $mentions_filter->setStringTranslation($this->getStringTranslationStub());
+
+    $test = function($input) use ($mentions_filter) {
+      return $mentions_filter->mentions_get_mentions($input);
+    };
+
+    $this->assertEquals($expected, $test($input));
+  }
+
+}
