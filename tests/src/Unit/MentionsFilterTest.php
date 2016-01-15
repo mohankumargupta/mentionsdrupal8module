543160dcbb704529cadd3ca03afdcb27ac4f9c6b
Mohan Gupta <mohangupta@live.com>
2016-01-15 23:46:49 +1100

    expected object wrong

--- tests/src/Unit/MentionsFilterTest.php
+++ tests/src/Unit/MentionsFilterTest.php
@@ -102,7 +102,7 @@ class MentionsFilterTest extends UnitTestCase {
 
     $this->configFactory->expects($this->once())
       ->method('get')
-      ->with('mentions.mentions_type')
+      ->with('mentions.mentions_type.')
       ->will($this->returnValue($this->config));
 
     $mentions_filter->setConfig($this->configFactory);
