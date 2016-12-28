6d3450ebb19d93130f5f05ebc60f873d5406312b
mohankumargupta <mohankumargupta@users.noreply.github.com>
2016-12-28 14:35:13 +1100

    Create MentionsUserList.php

---
+++ src/Controller/MentionsUserList.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mentions\Controller\MentionsUserList.
+ */
+
+namespace Drupal\mentions\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\Query\QueryInterface;
+
+/**
+ * Returns json for user names that are enabled.
+ */
+class MentionsUserList extends ControllerBase {
+
+  protected $queryInterface;
+
+  public function __construct(QueryInterface $queryinterface) {
+    $this->queryInterface = $queryinterface;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+    $container->get('entity.query')->get('user')
+    );
+  }
+
+  public function userList(Request $request) {
+    $usernids = $this->queryInterface
+                         ->condition('status', 1)
+                         ->sort('name')
+                         ->execute();
+    $users = entity_load_multiple('user', $usernids);
+    $userlist = array();
+
+    foreach ($users as $user) {
+      $userlist['userlist'][] = $user->getUsername();
+    }
+
+    $response = new JsonResponse($userlist);
+    return $response;
+  }
+  
+  public function userPrefixesAndSuffixes() {
+      
+  }
+
+}
+
